82591814d19d2969ef47c8f1d359325eeabfbd7c
steamedbao <wanruiray@hotmail.com>
2022-01-20 16:23:18 +0800

    remove abstractuser

--- backend/CRUD/models.py
+++ backend/CRUD/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 
-class User(AbstractUser):
-    name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(unique=True, null=True)
-    bio = models.TextField(null=True)
 
-    avatar = models.ImageField(null=True, default="avatar.svg")
+# class User(AbstractUser):
+#     name = models.CharField(max_length=200, null=True)
+#     email = models.EmailField(unique=True, null=True)
+#     bio = models.TextField(null=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+#     avatar = models.ImageField(null=True, default="avatar.svg")
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
 
 
 
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'CRUD.apps.CrudConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
-    'corsheaders',
     'rest_framework'
 ]
 
@@ -53,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
