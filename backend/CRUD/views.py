eb8a470091389c5d214921108be9855293e638dc
joseph <josephchong1009@gmail.com>
2022-01-20 15:53:38 +0800

    deleted unnecesary file, included CRUD functions

--- backend/CRUD/views.py
+++ backend/CRUD/views.py
@@ -1,16 +1,57 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Post
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from .models import Message, User
+from .forms import messageForm
 
-def home(request):
-    return HttpResponse("Hi")
 
-def editPost(request):
-    post =  Post.objects.all
-    context = {}
-    return render(request,'filename.html',context)
 
-# def deletePost(request):
 
+
+
+@login_required(login_url='login')
+def createPost(request):
+    form = messageForm()
+    post = Message.objects.all()
+    if request.method == 'POST':
+        post = request.POST.get('body')
+        
+        return redirect('home')
+
+    context = {'form': form, 'post': post}
+    # return render(request, 'base/room_form.html', context)
+
+
+@login_required(login_url='login')
+def updatePost(request):
+    user= User.objects.get(name=request.user)
+    form = messageForm()
+    post = Message.objects.all()
+    if request.user != Message.user:
+        return HttpResponse('Your are not allowed here!!')
+
+    if request.method == 'POST':
+        post = request.POST.get('body')
+        post.user = request.POST.get('user')
+       
+        post.save()
+        return redirect('home')
+
+    context = {'form': form, 'post': post}
+    # return render(request,'file' , context)
+
+
+@login_required(login_url='login')
+def deletePost(request):
+    post= Message.objects.get('body')
+
+    if request.user != post.user:
+        return HttpResponse('Your are not allowed here!!')
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+    # return render(request, 'file_name', {})
 
